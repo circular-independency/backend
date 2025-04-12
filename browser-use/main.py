c16a066edd846e864d1e3022bcd4dcb1407acfb7
PeterKeight <peter.kosem@gmail.com>
2025-04-12 21:01:23 +0200

    browser-use

---
+++ browser-use/main.py
@@ -0,0 +1,173 @@
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pydantic import SecretStr
+
+from browser_use import Agent, BrowserConfig
+from browser_use.browser.browser import Browser
+from browser_use.browser.context import BrowserContextConfig
+
+from typing import List
+from pydantic import BaseModel
+
+from browser_use import Agent, Controller
+
+from task import *
+
+load_dotenv()
+api_key = os.getenv('GEMINI_API_KEY')
+if not api_key:
+	raise ValueError('GEMINI_API_KEY is not set')
+
+
+class Ingredient(BaseModel):
+	name: str
+	quantity: str
+	price: float
+
+
+class ShoppingCart(BaseModel):
+	posts: List[Ingredient]
+
+
+controller = Controller(output_model=ShoppingCart)
+
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(api_key))
+
+browser = Browser(
+	config=BrowserConfig(
+		new_context_config=BrowserContextConfig(
+			viewport_expansion=0,
+			#allowed_domains=['mercatoronline.si', 'www.spar.si', 'hitrinakup.com']
+		)
+	)
+)
+
+async def run_search(task):
+	agent = Agent(
+		task=task,
+		llm=llm,
+		max_actions_per_step=4,
+		browser=browser,
+		controller=controller,
+	)
+
+	history = await agent.run(max_steps=25)
+	
+	result = history.final_result()
+	if result:
+		parsed: ShoppingCart = ShoppingCart.model_validate_json(result)
+		for post in parsed.posts:
+			print('\n--------------------------------')
+			print(f'Name:             {post.name}')
+			print(f'Quantity:         {post.quantity}')
+			print(f'Price:            {post.price}')
+	else:
+		print('No result')
+	
+	# wait 10 seconds before closing the browser
+	await asyncio.sleep(10)
+
+
+if __name__ == '__main__':
+	stores = {
+		"mercator": {
+			"link": "https://mercatoronline.si/"
+		},
+		"tus": {
+			"link": "https://hitrinakup.com/priporoceni"
+		},
+		"lidl": {
+			"link": "https://www.lidl.si/"
+		},
+		"spar": {
+			"link": "https://www.spar.si/online/"
+		}
+	}
+	# Example shopping list
+	shopping_list = {
+		"mercator": [
+			{
+				"name": "sladkor",
+				"quantity": "1kg",
+				"price": 1.5,
+				"vendor_id": "726009",
+				"source": "mercator"
+			},
+			{
+				"name": "moka",
+				"quantity": "1kg",
+				"price": 2.0,
+				"vendor_id": "21439",
+				"source": "mercator"
+			}
+		],
+		"spar": [
+			{
+				"name": "sladkor",
+				"quantity": "1kg",
+				"price": 1.5,
+				"vendor_id": "439782",
+				"source": "spar"
+			},
+			{
+				"name": "moka",
+				"quantity": "1kg",
+				"price": 2.0,
+				"vendor_id": "173873",
+				"source": "spar"
+			}
+		],
+		"tus": [
+			{
+				"name": "sladkor",
+				"quantity": "1kg",
+				"price": 1.5,
+				"vendor_id": "439782",
+				"source": "spar"
+			},
+			{
+				"name": "moka",
+				"quantity": "1kg",
+				"price": 2.0,
+				"vendor_id": "173873",
+				"source": "spar"
+			}
+		]
+	}
+
+	preowned_ingridients = [
+		{
+			"name": "piščanec",
+			"quantity": "1kg",
+			"price": 2.0,
+			"vendor_id": "12345",
+			"source": "mercator"
+		}
+	]
+
+	example = str([
+			{
+				"name": "sladkor",
+				"quantity": "1kg",
+				"price": 1.5
+			},
+			{
+				"name": "moka",
+				"quantity": "1kg",
+				"price": 2.0
+			}
+	])
+
+	load_dotenv()
+	username = os.getenv('STORE_USERNAME')
+	password = os.getenv('STORE_PASSWORD')
+
+	store = "tus"
+
+	task = generate_prompt(store, stores, username, password, shopping_list, preowned_ingridients, example)
+	print(task)
+	
+	asyncio.run(run_search(task))
---
+++ browser-use/task.py
@@ -0,0 +1,247 @@
+from dotenv import load_dotenv
+import os
+
+def shopping_list_to_string(shopping_list):
+    """
+    Convert the shopping list dictionary to a string format.
+    """
+
+    stringified = ""
+
+    for item in shopping_list:
+        stringified += f"- {item['name']} ({item['quantity']}) id: {item['vendor_id']}\n"
+
+    return stringified
+
+def preowned_ingridients_to_string(ingredients):
+    """
+    Convert the pre-owned ingredients list to a string format.
+    """
+
+    stringified = ""
+
+    for item in ingredients:
+        stringified += f"- {item['name']} ({item['quantity']})\n"
+
+    return stringified
+
+def generate_prompt_mercator(store, store_link, username, password, stringified_list, stringified_preowned, example):
+    """
+    Generate a prompt for the shopping agent for Mercator.
+    """
+    
+    task = f"""
+### Prompt for Shopping Agent - {store} Online Grocery Order
+
+**Objective:**
+Visit [{store}]({store_link}), search for the required grocery items, add them to the cart, select an appropriate delivery window, but do not complete the checkout process, stay on shopping cart page.
+
+**Important:**
+- Make sure that you don't buy more than it's needed for each article.
+- After your search, if you click  the "V KOŠARICO" button, it adds the item to the basket.
+- if you open the basket sidewindow menu, you can close it by clicking the "NADALJUJTE Z NAKUPOVANJEM" button on the top right. This will help you navigate easier.
+- if there is a pop up window, you can close it by clicking the "X" button on the top right corner of the pop up window.
+---
+
+### Step 1: Navigate to the Website
+- Open [{store}]({store_link}).
+- You should log in as {username} by clicking on the button "Prijavite se". The password is : {password}
+
+---
+
+### Step 2: Add Items to the Basket
+- Always firstly search for the item in the search bar by the ID parameter if it exists.
+- If the item is not found, search for it by name, always in Slovenian language.
+
+#### Shopping List:
+
+{stringified_list}
+
+At this stage, check the basket on the top right (indicates the price) and check if you bought the right items.
+
+#### Ingredients I already have (DO NOT purchase):
+{stringified_preowned}
+
+---
+
+### Step 3: Handling Unavailable Items
+- If an item is **out of stock** (**Ni na zalogi**), find the best alternative.
+
+---
+
+### Step 4: Stay on the Cart Page
+- proceed to the cart page ("Moja košarica") and check the items.
+- if all the items are correct, proceed to the checkout page ("Nadaljujte z naslednjim korakom").
+- if it's needed the username is: {username}
+- and the password is : {password}
+---
+
+### Step 5: Select Delivery Window
+- Select shipping to home ("Dostava na dom").
+- Select the delivery address (address should be Večna pot 113, 1000 Ljubljana)
+- Select the earliest available delivery window.
+- If the delivery window is not available, select the next available one.
+- Proceed with the checkout process ("Nadaljujte z naslednjim korakom") until the payment page, but do not complete the order.
+- Stay on the checkout page (where it says "Izberite način plačila izdelkov")
+
+### Step 5: Output Summary
+- While on the checkout page, output a summary including:
+- **Final list of items in cart** (including any substitutions).
+Example:
+{example}
+
+**Important:** Ensure efficiency and accuracy throughout the process."""
+
+    return task
+
+def generate_prompt_spar(store, store_link, username, password, stringified_list, stringified_preowned, example):
+    """
+    Generate a prompt for the shopping agent for Spar.
+    """
+    
+    task = f"""
+### Prompt for Shopping Agent - {store} Online Grocery Order
+
+**Objective:**
+Visit [{store}]({store_link}), search for the required grocery items, add them to the cart, select an appropriate delivery window, proceed to checkout but do not buy the cart.
+
+**Important:**
+- Make sure that you don't buy more than it's needed for each article.
+- After your search, if you click the "DODAJ V KOŠARICO" button or a button with the cart logo, it adds the item to the basket.
+---
+
+### Step 1: Navigate to the Website
+- Open [{store}]({store_link}).
+- You should log in as {username} by clicking on the button with silhuette logo and then proceed to log in (PRIJAVA). The password is : {password}
+
+---
+
+### Step 2: Add Items to the Basket
+- Always firstly search for the item in the search bar by the ID parameter if it exists.
+- If the item is not found, search for it by name, always in Slovenian language.
+
+#### Shopping List:
+
+{stringified_list}
+
+At this stage, check the basket on the top right (indicates the price) and check if you bought the right items.
+
+#### Ingredients I already have (DO NOT purchase):
+{stringified_preowned}
+
+---
+
+### Step 3: Handling Unavailable Items
+- If an item is **out of stock** (**Ni na zalogi**), find the best alternative.
+
+---
+
+### Step 4: Stay on the Cart Page
+- proceed to the cart page (logo of a cart) and check the items.
+- if you didnt get for at least 25€ of items, get additional of the same item until you reach 25€.
+- if all the items are correct, proceed to the checkout page ("Na blagajno").
+- if it's needed the username is: {username}
+- and the password is : {password}
+---
+
+### Step 5: Select Delivery Window
+- Select drive in delivery ("Lokacije in ure prevzema").
+- Select the delivery address (address should be Moskovska ulica 4, 1000 Ljubljana)
+- Select the earliest available delivery window.
+- If the delivery window is not available, select the next available one.
+- Proceed with the checkout process ("Naprej") until the payment page, but do not complete the order.
+- Stay on the checkout page (where it says "Izberite vrsto plačila")
+
+### Step 5: Output Summary
+- While on the checkout page, output a summary including:
+- **Final list of items in cart** (including any substitutions).
+Example:
+{example}
+
+**Important:** Ensure efficiency and accuracy throughout the process."""
+
+    return task
+
+
+def generate_prompt_tus(store, store_link, username, password, stringified_list, stringified_preowned, example):
+    """
+    Generate a prompt for the shopping agent for Tuš.
+    """
+    
+    task = f"""
+### Prompt for Shopping Agent - {store} Online Grocery Order
+
+**Objective:**
+Visit [{store}]({store_link}), search for the required grocery items, add them to the cart, select an appropriate delivery window, proceed to checkout but do not buy the cart.
+
+**Important:**
+- Make sure that you don't buy more than it's needed for each article.
+- After your search, if you click the "DODAJ V KOŠARICO" button or a button with the cart logo, it adds the item to the basket.
+---
+
+### Step 1: Navigate to the Website
+- Open [{store}]({store_link}).
+- You should log in as {username} by clicking on the button with silhuette logo or button ("Prijava") and then proceed to log in (PRIJAVA). The password is : {password}
+
+---
+
+### Step 2: Add Items to the Basket
+- Always firstly search for the item in the search bar by the ID parameter if it exists.
+- If the item is not found, search for it by name, always in Slovenian language.
+
+#### Shopping List:
+
+{stringified_list}
+
+At this stage, check the basket on the top right (indicates the price) and check if you bought the right items.
+
+#### Ingredients I already have (DO NOT purchase):
+{stringified_preowned}
+
+---
+
+### Step 3: Handling Unavailable Items
+- If an item is **out of stock** (**Ni na zalogi**), find the best alternative.
+
+---
+
+### Step 4: Stay on the Cart Page
+- proceed to the cart page (logo of a cart) and check the items.
+- if you didnt get for at least 25€ of items, get additional of the same item until you reach 25€.
+- if all the items are correct, proceed to the checkout page ("Zaključi nakup").
+- if it's needed the username is: {username}
+- and the password is : {password}
+---
+
+### Step 5: Select Delivery Window
+- Select drive in delivery ("Lokacije in ure prevzema").
+- Select the delivery address (address should be Pickup Tuš Vič, Cesta v Mestni log 84, Ljubljana)
+- Select the earliest available delivery window.
+- If the delivery window is not available, select the next available one.
+- Proceed with the checkout process ("Naprej") until the payment page, but do not complete the order.
+- Stay on the checkout page (where it says "Izberite vrsto plačila")
+
+### Step 5: Output Summary
+- While on the checkout page, output a summary including:
+- **Final list of items in cart** (including any substitutions).
+Example:
+{example}
+
+**Important:** Ensure efficiency and accuracy throughout the process."""
+
+    return task
+
+def generate_prompt(store, stores, username, password, shopping_list, preowned_ingredients, example):
+    """
+    Generate a prompt for the shopping agent based on the store and shopping list.
+    """
+
+    stringified_list = shopping_list_to_string(shopping_list[store])
+    stringified_preowned = preowned_ingridients_to_string(preowned_ingredients)
+    
+    if store == "mercator":
+        return generate_prompt_mercator(store, stores[store]["link"], username, password, stringified_list, stringified_preowned, example)
+    elif store == "spar":
+        return generate_prompt_spar(store, stores[store]["link"], username, password, stringified_list, stringified_preowned, example)
+    elif store == "tus":
+        return generate_prompt_spar(store, stores[store]["link"], username, password, stringified_list, stringified_preowned, example)
